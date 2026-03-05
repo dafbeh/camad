2c936daa85a095a3b25f004765f1964de48ede97
dafbeh <47498851+dafbeh@users.noreply.github.com>
2026-03-05 17:56:13 +0000

    Dynamic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import Link from "next/link"
+import { useState } from 'react';
+import { Menu, X } from "lucide-react";
+
 export default function Navbar() {
+  const navLinks = [
+    { label: "About", href: "/about" },
+    { label: "Services", href: "/services" },
+    { label: "Volunteer", href: "/volunteer" },
+    { label: "Contact", href: "/contact" },
+  ]
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="absolute top-0 left-1/2 -translate-x-1/2 flex max-w-6xl w-full
-          justify-between items-center z-10 py-5 lg:px-12 px-5">
+          justify-between items-center z-50 py-5 lg:px-12 px-5">
 
       {/* Left */}
       <div className="flex items-center gap-3 text-card select-none">
-        <div className="bg-primary rounded-full w-5 h-5 p-5 flex 
+        <div className="bg-primary rounded-full w-5 h-5 p-5 flex
               items-center justify-center">
           <span className="text-lg font-sans font-bold tracking-tight text-primary-foreground">C</span>
         </div>
@@ -17,7 +32,60 @@ export default function Navbar() {
       </div>
 
       {/* Right */}
-      <div>
+      <div className="flex z-50">
+        <div className="hidden md:flex gap-8 text-card/80 text-sm font-medium items-center tracking-wide">
+          {navLinks.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className="cursor-pointer hover:text-card transition-colors"
+            >
+              {link.label}
+            </Link>
+          ))}
+
+
+          <div className="flex flex-wrap items-center">
+            <Link
+              href="#volunteer"
+              className="rounded-full bg-primary px-5 py-3 font-semibold text-card
+                  transition-all hover:bg-primary/90 text-md"
+            >
+              Get Involved
+            </Link>
+          </div>
+        </div>
+
+        <div className="md:hidden flex items-center text-card"
+          onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <X /> : <Menu /> }
+        </div>
+
+        {isOpen && (
+          <div className="absolute top-20 left-0 bg-[#211914]/99 w-screen">
+            <div className="flex flex-col gap-8 px-8 pb-4 pt-8">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  className="text-card/80 text-sm font-medium items-center tracking-wide"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </div>
+
+            <div className="flex flex-wrap items-center p-5 pt-3">
+              <Link
+                href="#volunteer"
+                className="rounded-full bg-primary px-5 py-3 font-semibold text-card
+                  transition-all hover:bg-primary/90 text-md w-full text-center"
+              >
+                Get Involved
+              </Link>
+            </div>
+          </div>
+        )}
 
       </div>
     </div>
