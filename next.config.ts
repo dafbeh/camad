096e9f30a666af195f0a119d12fa75fdc9e304ba
dafbeh <47498851+dafbeh@users.noreply.github.com>
2026-03-07 23:58:58 +0000

    Build fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['en', 'cy'],  // English & Welsh
-    defaultLocale: 'en',
-  },
   images: {
     remotePatterns: [{ hostname: 'cdn.sanity.io' }],
   },
